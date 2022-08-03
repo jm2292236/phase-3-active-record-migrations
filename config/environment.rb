74585be8877e9de14c7b5f01ef24a09e22571b67
Jaime Montes de Oca <jaime0422@live.com>
2022-08-02 20:47:32 -0700

    Completed Assignment

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
+# We are using the development database
+# This RACK_ENV is a specific environment variable used by the sinatra-activerecord gem
+# to determine which database to connect to
 ENV["RACK_ENV"] ||= "development"
 
 require 'bundler/setup'
